a595390cad9061ba07a17b1e868a75cdcbdfae53
Andrey Mikhatsky <andrfas@gmail.com>
2016-02-08 01:20:32 +0200

    added city celection in regCompany

--- js/controllers/SelCityModalCntrl.js
+++ js/controllers/SelCityModalCntrl.js
@@ -1,11 +1,30 @@
-app.controller('SelCityModalCntrl', ['$scope', function($scope) {
-    $scope.selectedCities = [];
-    $scope.getCities = function() {
-        return ['Киев', 'Харьков', 'Луцк'];
+app.controller('SelCityModalCntrl', ['$scope', '$uibModalInstance', 'cities', function($scope, $uibModalInstance, cities) {
+    $scope.selectedCities = cities.slice();
+    $scope.searchStr = '';
+    $scope.getCities = function(name) {
+        name = name.toLowerCase();
+        var res = [];
+        cities.forEach(function(city) {
+            if (city.toLowerCase().indexOf(name) !== -1) {
+                res.push(city);
+            }
+        })
+        return res;
     }
 
     $scope.selectedCity;
     $scope.selectCity = function() {
         $scope.selectedCities.push($scope.selectedCity);
+        $scope.searchStr = '';
     }
+
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.selectedCities);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    var cities = ['Киев','Днепропетровск','Донецк','Запорожье','Кривой Рог','Львов','Луганск','Мариуполь','Николаев','Одесса','Севастополь','Симферополь','Харьков','Винница','Чернигов', 'Луцк'];
 }])
