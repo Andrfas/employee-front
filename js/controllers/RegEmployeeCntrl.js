bf2977d13054766667d836b57a39a526e6356bf4
NickolasBiletskiy <supernickola@ukr.net>
2016-02-09 20:28:12 +0200

    fixed bags, added error messages to each input field

--- js/controllers/RegEmployeeCntrl.js
+++ js/controllers/RegEmployeeCntrl.js
@@ -1,7 +1,27 @@
 app.controller('RegEmployeeCntrl', ['$scope', function($scope) {
-    $scope.name = '';
+    $scope.studInfo = {
+      name: '',
+      lastName: '',
+      email: '',
+      password: '',
+      repeatPass:''
+    };
+    $scope.checkPasswords = function(){
+        return $scope.studInfo.password === $scope.studInfo.repeatPass;
+    }
     $scope.stud = 'stud1';
-    $scope.changeEmplSelect = function(person){
-        $scope.stud = person;
+    $scope.changeEmplSelect = function(person, direction){ //direction next or prev
+
+        if (direction === 'next'){
+            if ($scope.checkPasswords()) {
+                $scope.stud = person;
+            }
+            else{
+                document.getElementById("errorField").innerHTML = "Different passwords"
+            }
+        }
+            else {
+            $scope.stud = person;
+        }
     }
 }])
