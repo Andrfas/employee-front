b627e8b1d9803b3254218ae20103e6471ae85618
Andrey Mikhatsky <andrfas@gmail.com>
2016-02-19 22:57:29 +0200

    implemented api for showing copany profile

--- js/controllers/CompanyProfileCntrl.js
+++ js/controllers/CompanyProfileCntrl.js
@@ -1,4 +1,18 @@
-app.controller('CompanyProfileCntrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-    console.log($routeParams.profileId);
+app.controller('CompanyProfileCntrl', ['$scope', '$routeParams', 'RegCompanySrvc', 'PopUpSrvc', function($scope, $routeParams, RegCompanySrvc, PopUpSrvc) {
+    $scope.companyId = $routeParams.profileId;
+    $scope.profile;
+
+    RegCompanySrvc.getCompany($scope.companyId)
+        .then(function(res) {
+            if(res.success) {
+                $scope.profile = res.data;
+                $scope.profile.cities = $scope.profile.cities.join('-');
+            } else {
+                PopUpSrvc.error('Error', res.msg);
+            }
+        })
+        .catch(function(err) {
+            PopUpSrvc.error('Error', err);
+        })
 }])
 
