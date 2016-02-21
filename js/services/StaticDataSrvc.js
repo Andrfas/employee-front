9624c33145b846b08a4ca46b148d70a5819ed88c
Andrey Mikhatsky <andrfas@gmail.com>
2016-02-21 12:43:29 +0200

    added service for static data

---
+++ js/services/StaticDataSrvc.js
@@ -0,0 +1,22 @@
+app.service('StaticDataSrvc', [function() {
+
+    // Categories and subcategories. Field categ in each of subcategories is needed for proper displayingrp puose. @Andrfas
+    this.categories = [
+        {
+            name: 'IT',
+            subcategories: [
+                {categ: 'IT', subCateg:'Desktop Software Development'},
+                {categ: 'IT', subCateg:'Ecommerce Development'},
+                {categ: 'IT', subCateg:'Game Development'},
+                {categ: 'IT', subCateg:'Mobile Development'},
+                {categ: 'IT', subCateg:'Product Management'},
+                {categ: 'IT', subCateg:'QA & Testing'},
+                {categ: 'IT', subCateg:'Scripts & Utilities'},
+                {categ: 'IT', subCateg:'Web Development'},
+                {categ: 'IT', subCateg:'Web & Mobile Design'},
+                {categ: 'IT', subCateg:'Other - Software Development'}
+            ]
+        }        
+    ]
+    
+}]);
