e053b28740140c9a69a652c90fd6b19728e8b3fb
NickolasBiletskiy <supernickola@ukr.net>
2016-02-23 16:56:44 +0200

    binded angular to addEducation modal

--- js/controllers/RegEmployeeCntrl.js
+++ js/controllers/RegEmployeeCntrl.js
@@ -1,12 +1,14 @@
 app.controller('RegEmployeeCntrl', ['$scope', '$uibModal', 'StaticDataSrvc', 'RegEmployeeSrvc', 'PopUpSrvc', function($scope, $uibModal, StaticDataSrvc, RegEmployeeSrvc, PopUpSrvc) {
     $scope.languages = []; //fields name, level
+    $scope.educations = [];
     $scope.studInfo = {
-      name: '',
-      lastName: '',
-      email: '',
-      password: '',
-      repeatPass:'',
-      languagesArr:[]
+        name: '',
+        lastName: '',
+        email: '',
+        password: '',
+        repeatPass:'',
+        languagesArr:[],
+        educationsArr: []
     };
 
     $scope.isConfirmPasswordMatch = function() {
@@ -43,6 +45,10 @@ app.controller('RegEmployeeCntrl', ['$scope', '$uibModal', 'StaticDataSrvc', 'Re
         StaticDataSrvc.languages.push($scope.languages[index].name);
         $scope.languages.splice(index,1);
     }
+
+    $scope.removeEducation = function(index){
+        $scope.educations.splice(index,1);
+    }
     //opening modals
     $scope.openSelectLanguageWindow = function(){
         var instance = $uibModal.open({
@@ -66,7 +72,9 @@ app.controller('RegEmployeeCntrl', ['$scope', '$uibModal', 'StaticDataSrvc', 'Re
             keyboard: true,
             templateUrl: '../templates/modals/addEducationModal.html',
             controller: 'SelEducationModalCntrl'
-
+        });
+        instance.result.then(function (education){
+            $scope.educations.push(education);
         })
     }
     $scope.openAddSkillWindow = function() {
--- js/controllers/SelEducationModalCntrl.js
+++ js/controllers/SelEducationModalCntrl.js
@@ -1,9 +1,50 @@
 app.controller('SelEducationModalCntrl', ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
+    $scope.startYear = new Date();
+    $scope.finishYear = new Date();
+
+    $scope.education = {
+        institution: '',
+        startYear: '',
+        finishYear: '',
+        faculty: '',
+        degree: ''
+    }
+
+    $scope.mode = 'year'; //mode of datepicker
+
+    $scope.pack = function(){ //creates education object
+        $scope.education.startYear = $scope.startYear.getFullYear();
+        $scope.education.finishYear = $scope.finishYear.getFullYear();
+    }
     $scope.save = function(){
-        $uibModalInstance.close();
+        $scope.pack();
+        //console.log($scope.education);
+        $uibModalInstance.close($scope.education);
     }
 
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
     };
+
+    $scope.openStart = function(){
+        $scope.startPopup.opened = true;
+    }
+    $scope.openFinish = function(){
+        $scope.finishPopup.opened = true;
+    }
+
+    $scope.finishDisabled = function(date, mode){
+        return mode === $scope.mode && date.getFullYear()<$scope.startYear.getFullYear();
+    }
+    $scope.startDisabled = function(date, mode){
+        return mode === $scope.mode && date.getFullYear()>new Date().getFullYear();
+    }
+
+    $scope.startPopup = {
+        opened: false
+    }
+    $scope.finishPopup = {
+        opened: false
+    }
+
 }])
