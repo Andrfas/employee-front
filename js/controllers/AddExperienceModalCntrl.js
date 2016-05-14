fc336b4ac7256a8947512438bc40b5c1ddf7fc3d
gusa4okgrr <gusa4ok.1996@gmail.com>
2016-05-14 19:22:01 +0300

    Fixed ExperienceModal

--- js/controllers/AddExperienceModalCntrl.js
+++ js/controllers/AddExperienceModalCntrl.js
@@ -1,10 +1,44 @@
 app.controller('AddExperienceModalCntrl', ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
+    $scope.experience = {
+    	title: '',
+    	yearsFrom: '',
+    	yearsTo: '',
+    	degree: ''
+    }
+
     $scope.save = function(){
-        $uibModalInstance.close();
+        $uibModalInstance.close($scope.experience);
     }
 
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
     };
+
+    $scope.datepickerConf = {
+        mode: 'year',
+        opened: false,
+        options:{
+            minDate: new Date(1960, 1, 1),
+            maxDate: new Date(2003, 1, 1),
+            initDate: new Date(1993, 1, 1),
+            showWeeks: false,
+        },
+        open: function(){
+            this.opened = true;
+        }
+    }
+    $scope.datepickerConf2 = {
+        mode: 'year',
+        opened: false,
+        options:{
+            minDate: new Date(1960, 1, 1),
+            maxDate: new Date(2003, 1, 1),
+            initDate: new Date(1993, 1, 1),
+            showWeeks: false
+        },
+        open: function(){
+            this.opened = true;
+        }
+    }
 }])
 
