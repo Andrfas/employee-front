e9051b374ad11313982b4de37b67a94a52491d0f
gusa4okgrr <gusa4ok.1996@gmail.com>
2016-05-14 17:00:53 +0300

    Added option to change availability + back-end

--- js/services/RegEmployeeSrvc.js
+++ js/services/RegEmployeeSrvc.js
@@ -24,6 +24,21 @@ app.service('RegEmployeeSrvc', ['ReqHandlingSrvc', '$q', function(ReqHandlingSrv
                     call.reject(err);
                 })
             return call.promise;
+        },
+
+        updateEmployee: function(id, data){
+            var url = '/employee/' + id;
+            var call = $q.defer();
+            ReqHandlingSrvc.patch(url, data)
+                .then(function(res) {
+                    console.log(res);
+                    call.resolve(res);
+                })
+                .catch(function(err) {
+                    console.log(err);
+                    call.reject(err);
+                })
+            return call.promise;
         }
     }
 }]);
