37ba50c9c2876139d6bf92ed4e80e218bc897f82
gusa4okgrr <gusa4ok.1996@gmail.com>
2016-05-15 07:39:34 +0300

    Fixed applying

--- js/controllers/AdvertCntrl.js
+++ js/controllers/AdvertCntrl.js
@@ -1,7 +1,9 @@
-app.controller('AdvertCntrl', ['$scope', '$routeParams', 'AdvertSrvc', function($scope, $routeParams, AdvertSrvc) {
+app.controller('AdvertCntrl', ['$scope', '$routeParams', 'AdvertSrvc', 'PopUpSrvc', '$uibModal', '$location', function($scope, $routeParams, AdvertSrvc, PopUpSrvc, $uibModal, $location) {
     $scope.advertId = $routeParams.advertId;
     $scope.advert;
 
+    if (localStorage.getItem('clientType') === 'company') $scope.companyType = true; else $scope.companyType = false;
+    console.log($scope.companyType);
     $scope.getAdvertData = function(advertId) {
         var reqObj = {
             advertId: advertId
@@ -16,5 +18,36 @@ app.controller('AdvertCntrl', ['$scope', '$routeParams', 'AdvertSrvc', function(
             })
     }
     $scope.getAdvertData($scope.advertId);
+
+    $scope.submitProposalModal = function(id){
+        var submittedProposal = null;
+        var instance = $uibModal.open({
+            animation: true,
+            keyboard:true,
+            templateUrl: '../templates/modals/addSubmitProposalModal.html',
+            controller: 'SubmitProposalCntrl',
+            resolve: {
+                prop: function () {
+                    return submittedProposal;
+                }
+            }
+        });
+
+        instance.result.then(function (prop) {
+            var toSend = {}
+            toSend.employeeId = localStorage.getItem('clientId');
+            toSend.letter = prop;
+            toSend.advertId = id;
+            AdvertSrvc.submitProposal(toSend)
+                .then(function(res){
+                    if (res.status === 200) PopUpSrvc.success('Proposal Submitted!', 'You successfully apply for this purpose');
+                })
+        });
+    }
+
+    $scope.showAllApplyers = function(){
+        $location.path('/applicants');
+        AdvertSrvc.advertId = $scope.advertId;
+    }
 }])
 
